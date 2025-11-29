d7d0254c55ebfe3005f4abf69342cf1ff3464b88
iris <scout20010@gmail.com>
2025-11-29 14:57:32 +0300

    feat: add ui-component Illustration

---
+++ src/components/ui/Illustration/Illustration.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { Illustration } from './Illustration'
+import illustration from '../../../assets/illustrations/cta.png'
+
+const meta = {
+  title: 'UI/Illustration',
+  component: Illustration,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Illustration>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    src: illustration,
+    alt: 'illustration'
+  }
+}
