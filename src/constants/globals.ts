e1739160734aa16b2757dc2235f141ccee4f2f4a
iris <scout20010@gmail.com>
2025-11-29 14:58:45 +0300

    feat: add constants

---
+++ src/constants/globals.ts
@@ -0,0 +1,1 @@
+export const SVG = 'http://www.w3.org/2000/svg'
---
+++ src/constants/navigationLinks.ts
@@ -0,0 +1,7 @@
+export const NAVIGATION_LINKS = [
+  { to: '/about_us', label: 'About us' },
+  { to: '/services', label: 'Services' },
+  { to: '/use_cases', label: 'Use Cases' },
+  { to: '/pricing', label: 'Pricing' },
+  { to: '/blog', label: 'Blog' },
+]
